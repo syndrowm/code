5bb9917e90bf31a1e3514a0d71ebd43b80e9cfe9
syndrowm <syndrowm@gmail.com>
2012-02-27 20:37:07 -0700

    yay code

---
+++ vm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+IFS="
+"
+VM=$1
+IP=$2
+PORT=$3
+VMX_DIR="/Users/evan/Documents/Virtual Machines.localized/"
+VMRUN="/Applications/VMware Fusion.app/Contents/Library/vmrun"
+NCAT="/usr/local/bin/ncat"
+
+RUNNING=`${VMRUN} list | grep "${VM}"`
+
+if [[ -z "${RUNNING}" ]];then
+	echo "[+] starting ${VMX}"
+	${VMRUN} -T fusion start ${VMX_DIR}/${VM} nogui
+fi
+
+count=0
+while !	${NCAT} $IP $PORT 
+do
+	sleep 5
+	if [ $count > 10 ];then
+		exit
+	fi
+done
