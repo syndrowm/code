6a1d6c2b0d3a29bc1bde16217b7ef93d92d42475
syndrowm <syndrowm@gmail.com>
2012-02-27 20:58:06 -0700

    add comments

--- vm.sh
+++ vm.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# check if a vm is booted, then forward ssh too it via ncat
+# sample .ssh/config
+# ~/.ssh/config
+# Host vmhost
+#    Hostname = IP
+#    User = username
+#    ProxyCommand = PATH/to/vm.sh vmname.vmx %h %p
+#
 IFS="
 "
 VM=$1
